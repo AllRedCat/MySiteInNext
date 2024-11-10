d1b6b32a13883ca37aef5199436c9f167478ef9e
Gabriel <leonismogabriel@gmail.com>
2024-11-10 13:31:49 -0300

    Pages e modelo de footer no '/'

--- app/page.tsx
+++ app/page.tsx
@@ -1,81 +1,69 @@
 import Image from "next/image";
+import Link from "next/link";
+
+const fotoStyle = {
+  borderRadius: "50%"
+}
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_100px] grid-cols-[100px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-2 sm:p-20 font-[family-name:var(--font-geist-sans)] text-foreground">
+    <div className="grid grid-rows-[20px_1fr_100px] grid-cols-[100px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-2 sm:p-8 font-[family-name:var(--font-geist-sans)] text-foreground">
       <header className="row-start-2 col-start-1 flex flex-col items-start justify-center gap-6">
-        <div>
-          <a href="">
-            <Image
-              src="/gabsFaria.svg"
-              alt="Next.js logo"
-              width={80}
-              height={38}
-              priority
-            />
-          </a>
-        </div>
         <nav>
           <ul className="flex flex-col gap-6">
             <li>
-              <a
+              <Link href="/"
                 className="flex items-center hover:underline hover:underline-offset-4"
-                href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                target="_blank"
                 rel="noopener noreferrer"
               >
-                Docs
-              </a>
+                Home
+              </Link>
             </li>
             <li>
-              <a
-                className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                href="https://github.com/vercel/next.js/tree/canary/examples"
-                target="_blank"
+              <Link
+                className="flex items-center hover:underline hover:underline-offset-4"
+                href="/projetos"
                 rel="noopener noreferrer"
               >
-                Examples
-              </a>
+                Projetos
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+                // href="https://github.com/vercel/next.js/tree/canary/examples"
+                href="/contato"
+                rel="noopener noreferrer"
+              >
+                Contato
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+                href="/sobre"
+                rel="noopener noreferrer"
+              >
+                Sobre mim
+              </Link>
             </li>
           </ul>
         </nav>
       </header>
       <main className="flex flex-col gap-8 row-start-2 col-start-2 items-center sm:items-start">
         <Image
-          className="dark:invert"
-          src="/next.svg"
+          src="/foto.jpg"
           alt="Next.js logo"
           width={180}
           height={38}
+          style={fotoStyle}
           priority
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        <div>
+            Me chamo Gabriel
+        </div>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -83,6 +71,13 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             Read our docs
+          </a>
+          <a
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            href="/contato"
+            rel="noopener noreferrer"
+          >
+            Entre em contato
           </a>
         </div>
       </main>
@@ -95,12 +90,13 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/file.svg"
+            className="dark:invert"
+            src="/github.png"
             alt="File icon"
-            width={16}
-            height={16}
+            width={30}
+            height={30}
           />
-          Learn
+          GitHub
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
@@ -110,12 +106,13 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/window.svg"
+            className="dark:invert"
+            src="/linkedin.png"
             alt="Window icon"
-            width={16}
-            height={16}
+            width={30}
+            height={30}
           />
-          Examples
+          Linkedin
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
@@ -125,12 +122,14 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/globe.svg"
+            className="dark:invert"
+            src="/instagram.png"
             alt="Globe icon"
-            width={16}
-            height={16}
+            width={30}
+            height={30}
           />
-          Go to nextjs.org →
+          Instagram
+          {/* Go to nextjs.org → */}
         </a>
       </footer>
     </div>
